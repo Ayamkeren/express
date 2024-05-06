da2dd93e2688aaea06ace43b0727f34419973d9d
abyghails <abyghail10@gmail.com>
2024-05-06 19:23:02 +0700

    Pengubahan Text

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Woi test"));
+app.get("/", (req, res) => res.send("Hello Dava"));
 
 app.listen(3000, () => console.log("Server ready on port 3000"));
 
